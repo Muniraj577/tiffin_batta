c30952b9c2b7d0ac6c2fc2d2909eab3c2e3bbe52
Anshu Mallik <anshumallik143@gmail.com>
2022-11-21 20:47:58 +0545

    breadcrumb added

---
+++ cart.php
@@ -0,0 +1,11 @@
+<?php
+$pageTitle = "Cart";
+?>
+<?php include 'includes/header.php'?>
+<?php include 'includes/breadcrumb.php'?>
+
+<?php
+include 'includes/footer.php';
+include 'includes/script.php'
+
+?>
